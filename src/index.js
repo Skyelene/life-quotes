407a5fd74bb46b1eb8862ce6416b82a8820220c9
Rach <152854614+Skyelene@users.noreply.github.com>
2024-02-13 13:44:25 +0100

    Added Basic JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateQuote(event) {
+  event.preventDefault();
 
+  new Typewriter("#quote", {
+    strings: ["Sing like no one’s listening,"],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let quoteFormElement = document.querySelector("#quote-generator-form");
+quoteFormElement.addEventListener("submit", generateQuote);
